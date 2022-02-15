d2a06b15b96fe1fc1852be70f7a5061fd5f73d22
sonhy02 <60101129+sonhy02@users.noreply.github.com>
2022-02-15 23:06:59 +0900

    Add files via upload

---
+++ practice1.kt
@@ -0,0 +1,82 @@
+fun main() 
+{   var nl: Int; var el: Int; var ql: Int;  
+    print("일반공격 레벨을 입력하세요: ")
+    var nl = readLine()!!.toInt()
+    print("원소전투 레벨을 입력하세요: ")
+    var el = readLine()!!.toInt()
+    print("원소폭발 레벨을 입력하세요: ")
+    var ql = readLine()!!.toInt()
+
+ }
+
+ fun cal(nl: Int, el: Int, qla: Int)
+ {
+   val nl1 = 2.11
+   val nl2 = 2.283
+   val nl3 = 2.455
+   val nl4 = 2.7 
+   val nl5 = 2.872
+   val nl6 = 3.068
+   val nl7 = 3.339
+   val nl8 = 2.608
+   val nl9 = 3.879
+   val nl10 = 4.174
+   val nl11 = 4.469
+   val nl12 = 4.764
+   val nl13 = 5.058
+   val nl14 = 5.35
+   val nl15 = 5.645
+
+   val e1l = 2.39
+   val el2 = 2.57
+   val el3 = 2.75
+   val el4 = 2.99
+   val el5 = 3.17
+   val el6 = 3.35
+   val el7 = 3.59
+   val el8 = 3.83
+   val el9 = 4.07
+   val el10 = 4.31
+   val el11 = 4.55
+   val el12 = 4.78
+   val el13 = 5.08
+   val el14 = 5.38
+   val el15 = 5.68
+
+   val qla1 = 1.12
+   val qla2 = 1.21 
+   val qla3 = 1.29
+   val qla4 = 1.40
+   val qla5 = 1.49
+   val qla6 = 1.57
+   val qla7 = 1.68
+   val qla8 = 1.80
+   val qla9 = 1.91
+   val qla10 = 2.02
+   val qla11 = 2.13
+   val qla12 = 2.25
+   val qla13 = 2.39
+   val qla14 = 2.53
+   val qla15 = 2.67
+
+   val qlb1 = 1.68
+   val qlb2 = 1.81
+   val qlb3 = 1.94
+   val qlb4 = 2.11
+   val qlb5 = 2.23
+   val qlb6 = 2.36
+   val qlb7 = 2.53
+   val qlb8 = 2.70
+   val qlb9 = 2.86
+   val qlb10 = 3.03
+   val qlb11 = 3.20
+   val qlb12 = 3.37
+   val qlb13 = 3.58
+   val qlb14 = 3.79
+   val qlb15 = 4.00 
+  
+    val nk: Int = nl@nl; 
+    println("@nk")
+ 
+  
+ }
